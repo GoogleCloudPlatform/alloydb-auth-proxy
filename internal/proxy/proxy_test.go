783db6aba3d408fa57d7b86db895fae1f97583c9
Eno Compton <enocom@google.com>
2022-05-31 11:01:21 -0600

    feat: add support for unix sockets (#44)
    This is an adaptation from https://github.com/GoogleCloudPlatform/cloudsql-proxy/pull/1182

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -16,8 +16,10 @@ package proxy_test
 
 import (
 	"context"
+	"io/ioutil"
 	"net"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"cloud.google.com/go/alloydbconn"
@@ -28,9 +30,10 @@ import (
 type fakeDialer struct{}
 
 type testCase struct {
-	desc      string
-	in        *proxy.Config
-	wantAddrs []string
+	desc          string
+	in            *proxy.Config
+	wantTCPAddrs  []string
+	wantUnixAddrs []string
 }
 
 func (fakeDialer) Dial(ctx context.Context, inst string, opts ...alloydbconn.DialOption) (net.Conn, error) {
@@ -41,10 +44,25 @@ func (fakeDialer) Close() error {
 	return nil
 }
 
+func createTempDir(t *testing.T) (string, func()) {
+	testDir, err := ioutil.TempDir("", "*")
+	if err != nil {
+		t.Fatalf("failed to create temp dir: %v", err)
+	}
+	return testDir, func() {
+		if err := os.RemoveAll(testDir); err != nil {
+			t.Logf("failed to cleanup temp dir: %v", err)
+		}
+	}
+}
+
 func TestClientInitialization(t *testing.T) {
 	ctx := context.Background()
-	cluster1 := "/projects/proj/locations/region/clusters/clust/instances/inst1"
-	cluster2 := "/projects/proj/locations/region/clusters/clust/instances/inst2"
+	testDir, cleanup := createTempDir(t)
+	defer cleanup()
+	inst1 := "/projects/proj/locations/region/clusters/clust/instances/inst1"
+	inst2 := "/projects/proj/locations/region/clusters/clust/instances/inst2"
+	wantUnix := "proj.region.clust.inst1"
 
 	tcs := []testCase{
 		{
@@ -53,11 +71,11 @@ func TestClientInitialization(t *testing.T) {
 				Addr: "127.0.0.1",
 				Port: 5000,
 				Instances: []proxy.InstanceConnConfig{
-					{Name: cluster1},
-					{Name: cluster2},
+					{Name: inst1},
+					{Name: inst2},
 				},
 			},
-			wantAddrs: []string{"127.0.0.1:5000", "127.0.0.1:5001"},
+			wantTCPAddrs: []string{"127.0.0.1:5000", "127.0.0.1:5001"},
 		},
 		{
 			desc: "with instance address",
@@ -65,10 +83,10 @@ func TestClientInitialization(t *testing.T) {
 				Addr: "1.1.1.1", // bad address, binding shouldn't happen here.
 				Port: 5000,
 				Instances: []proxy.InstanceConnConfig{
-					{Addr: "0.0.0.0", Name: cluster1},
+					{Addr: "0.0.0.0", Name: inst1},
 				},
 			},
-			wantAddrs: []string{"0.0.0.0:5000"},
+			wantTCPAddrs: []string{"0.0.0.0:5000"},
 		},
 		{
 			desc: "with instance port",
@@ -76,10 +94,10 @@ func TestClientInitialization(t *testing.T) {
 				Addr: "127.0.0.1",
 				Port: 5000,
 				Instances: []proxy.InstanceConnConfig{
-					{Name: cluster1, Port: 6000},
+					{Name: inst1, Port: 6000},
 				},
 			},
-			wantAddrs: []string{"127.0.0.1:6000"},
+			wantTCPAddrs: []string{"127.0.0.1:6000"},
 		},
 		{
 			desc: "with global port and instance port",
@@ -87,11 +105,11 @@ func TestClientInitialization(t *testing.T) {
 				Addr: "127.0.0.1",
 				Port: 5000,
 				Instances: []proxy.InstanceConnConfig{
-					{Name: cluster1},
-					{Name: cluster2, Port: 6000},
+					{Name: inst1},
+					{Name: inst2, Port: 6000},
 				},
 			},
-			wantAddrs: []string{
+			wantTCPAddrs: []string{
 				"127.0.0.1:5000",
 				"127.0.0.1:6000",
 			},
@@ -102,13 +120,51 @@ func TestClientInitialization(t *testing.T) {
 				Addr: "127.0.0.1",
 				Port: 5432, // default port
 				Instances: []proxy.InstanceConnConfig{
-					{Name: cluster1},
-					{Name: cluster2},
+					{Name: inst1},
+					{Name: inst2},
 				},
 			},
-			wantAddrs: []string{
+			wantTCPAddrs: []string{
 				"127.0.0.1:5432",
 				"127.0.0.1:5433",
+			},
+		},
+		{
+			desc: "with a Unix socket",
+			in: &proxy.Config{
+				UnixSocket: testDir,
+				Instances: []proxy.InstanceConnConfig{
+					{Name: inst1},
+				},
+			},
+			wantUnixAddrs: []string{
+				filepath.Join(testDir, wantUnix, ".s.PGSQL.5432"),
+			},
+		},
+		{
+			desc: "with a global TCP host port and an instance Unix socket",
+			in: &proxy.Config{
+				Addr: "127.0.0.1",
+				Port: 5000,
+				Instances: []proxy.InstanceConnConfig{
+					{Name: inst1, UnixSocket: testDir},
+				},
+			},
+			wantUnixAddrs: []string{
+				filepath.Join(testDir, wantUnix, ".s.PGSQL.5432"),
+			},
+		},
+		{
+			desc: "with a global Unix socket and an instance TCP port",
+			in: &proxy.Config{
+				Addr:       "127.0.0.1",
+				UnixSocket: testDir,
+				Instances: []proxy.InstanceConnConfig{
+					{Name: inst1, Port: 5000},
+				},
+			},
+			wantTCPAddrs: []string{
+				"127.0.0.1:5000",
 			},
 		},
 	}
@@ -120,10 +176,10 @@ func TestClientInitialization(t *testing.T) {
 				Addr: "::1",
 				Port: 5000,
 				Instances: []proxy.InstanceConnConfig{
-					{Name: cluster1},
+					{Name: inst1},
 				},
 			},
-			wantAddrs: []string{"[::1]:5000"},
+			wantTCPAddrs: []string{"[::1]:5000"},
 		})
 	}
 
@@ -134,8 +190,19 @@ func TestClientInitialization(t *testing.T) {
 				t.Fatalf("want error = nil, got = %v", err)
 			}
 			defer c.Close()
-			for _, addr := range tc.wantAddrs {
+			for _, addr := range tc.wantTCPAddrs {
 				conn, err := net.Dial("tcp", addr)
+				if err != nil {
+					t.Fatalf("want error = nil, got = %v", err)
+				}
+				err = conn.Close()
+				if err != nil {
+					t.Logf("failed to close connection: %v", err)
+				}
+			}
+
+			for _, addr := range tc.wantUnixAddrs {
+				conn, err := net.Dial("unix", addr)
 				if err != nil {
 					t.Fatalf("want error = nil, got = %v", err)
 				}
@@ -146,4 +213,31 @@ func TestClientInitialization(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestClientInitializationWorksRepeatedly(t *testing.T) {
+	// The client creates a Unix socket on initial startup and does not remove
+	// it on shutdown. This test ensures the existing socket does not cause
+	// problems for a second invocation.
+	ctx := context.Background()
+	testDir, cleanup := createTempDir(t)
+	defer cleanup()
+
+	in := &proxy.Config{
+		UnixSocket: testDir,
+		Instances: []proxy.InstanceConnConfig{
+			{Name: "/projects/proj/locations/region/clusters/clust/instances/inst1"},
+		},
+	}
+	c, err := proxy.NewClient(ctx, fakeDialer{}, &cobra.Command{}, in)
+	if err != nil {
+		t.Fatalf("want error = nil, got = %v", err)
+	}
+	c.Close()
+
+	c, err = proxy.NewClient(ctx, fakeDialer{}, &cobra.Command{}, in)
+	if err != nil {
+		t.Fatalf("want error = nil, got = %v", err)
+	}
+	c.Close()
 }
