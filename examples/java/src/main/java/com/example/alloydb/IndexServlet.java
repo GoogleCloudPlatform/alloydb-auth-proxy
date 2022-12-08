7b9b7ba30e7c5c75c295d4293273d94b35710187
Shubha Rajan <shubhadayini@google.com>
2022-12-08 15:18:40 -0800

    chore: add connectivity sample for Java (#185)
    Co-authored-by: jackwotherspoon <jackwoth@google.com>

---
+++ examples/java/src/main/java/com/example/alloydb/IndexServlet.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.alloydb;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Nullable;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@SuppressFBWarnings(
+    value = {"SE_NO_SERIALVERSIONID", "WEM_WEAK_EXCEPTION_MESSAGING"},
+    justification = "Not needed for IndexServlet, Exception adds context")
+@WebServlet(name = "Index", value = "")
+public class IndexServlet extends HttpServlet {
+
+  private static final Logger LOGGER = Logger.getLogger(IndexServlet.class.getName());
+  private static final String testingTableName = System.getProperty("testingTableName");
+
+  public TemplateData getTemplateData(
+      DataSource pool, String tableName) throws ServletException {
+    try {
+      return TemplateData.getTemplateData(pool, tableName);
+    } catch (SQLException ex) {
+      throw new ServletException(ex);
+    }
+  }
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException, ServletException {
+    // Extract the pool from the Servlet Context, reusing the one that was created
+    // in the ContextListener when the application was started
+    DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");
+    String tableName = testingTableName;
+    if (tableName == null) {
+      tableName = "votes";
+    }
+    TemplateData templateData = getTemplateData(pool, tableName);
+
+    // Add variables and render the page
+    req.setAttribute("tabCount", templateData.tabCount);
+    req.setAttribute("spaceCount", templateData.spaceCount);
+    req.setAttribute("recentVotes", templateData.recentVotes);
+    req.getRequestDispatcher("/index.jsp").forward(req, resp);
+  }
+
+  // Used to validate user input. All user provided data should be validated and sanitized before
+  // being used something like a SQL query. Returns null if invalid.
+  @Nullable
+  private String validateTeam(String input) {
+    if (input != null) {
+      input = input.toUpperCase(Locale.ENGLISH);
+      // Must be either "TABS" or "SPACES"
+      if (!"TABS".equals(input) && !"SPACES".equals(input)) {
+        return null;
+      }
+    }
+    return input;
+  }
+
+  @SuppressFBWarnings(
+      value = {"SERVLET_PARAMETER", "XSS_SERVLET"},
+      justification = "Input is validated and sanitized.")
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String tableName = testingTableName;
+    if (tableName == null) {
+      tableName = "votes";
+    }
+
+    // Get the team from the request and record the time of the vote.
+    String team = validateTeam(req.getParameter("team"));
+    Timestamp now = new Timestamp(new Date().getTime());
+    if (team == null) {
+      resp.setStatus(400);
+      resp.getWriter().append("Invalid team specified.");
+      return;
+    }
+
+    // Reuse the pool that was created in the ContextListener when the Servlet started.
+    DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");
+    // [START alloydb_servlet_connection]
+    // Using a try-with-resources statement ensures that the connection is always released back
+    // into the pool at the end of the statement (even if an error occurs)
+    try (Connection conn = pool.getConnection()) {
+
+      // PreparedStatements can be more efficient and project against injections.
+      String stmt = String.format(
+          "INSERT INTO %s (time_cast, candidate) VALUES (?, ?);", tableName);
+      try (PreparedStatement voteStmt = conn.prepareStatement(stmt);) {
+        voteStmt.setTimestamp(1, now);
+        voteStmt.setString(2, team);
+
+        // Finally, execute the statement. If it fails, an error will be thrown.
+        voteStmt.execute();
+      }
+    } catch (SQLException ex) {
+      // If something goes wrong, handle the error in this section. This might involve retrying or
+      // adjusting parameters depending on the situation.
+      // [START_EXCLUDE]
+      LOGGER.log(Level.WARNING, "Error while attempting to submit vote.", ex);
+      resp.setStatus(500);
+      resp.getWriter()
+          .write(
+              "Unable to successfully cast vote! Please check the application "
+                  + "logs for more details.");
+      // [END_EXCLUDE]
+    }
+    // [END alloydb_servlet_connection]
+
+    resp.setStatus(200);
+    resp.getWriter().printf("Vote successfully cast for '%s' at time %s!%n", team, now);
+  }
+}
---
+++ examples/java/src/test/java/com/example/alloydb/TestIndexServlet.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.alloydb;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TestIndexServlet {
+
+  private static List<String> requiredEnvVars =
+      Arrays.asList("DB_USER", "DB_PASS", "DB_NAME", "DB_HOST", "DB_PORT");
+
+  private static DataSource pool;
+  private static String tableName;
+
+  public static void checkEnvVars() {
+    // Check that required env vars are set
+    requiredEnvVars.forEach((varName) -> {
+      assertWithMessage(
+          String.format("Environment variable '%s' must be set to perform these tests.", varName))
+          .that(System.getenv(varName)).isNotEmpty();
+    });
+  }
+
+  private static void createTable(DataSource pool) throws SQLException {
+    // Safely attempt to create the table schema.
+    tableName = String.format("votes_%s", UUID.randomUUID().toString().replace("-", ""));
+    try (Connection conn = pool.getConnection()) {
+      String stmt =
+          "CREATE TABLE IF NOT EXISTS "
+              + tableName
+              + " ( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL,"
+              + " candidate CHAR(6) NOT NULL,"
+              + " PRIMARY KEY (vote_id) );";
+      try (PreparedStatement createTableStatement = conn.prepareStatement(stmt);) {
+        createTableStatement.execute();
+      }
+    }
+  }
+
+
+  @BeforeClass
+  public static void createPool() throws SQLException {
+    checkEnvVars();
+    HikariConfig config = new HikariConfig();
+
+    config.setJdbcUrl(String.format("jdbc:postgresql://%s:%s/%s", 
+        System.getenv("DB_HOST"), 
+        System.getenv("DB_PORT"), 
+        System.getenv("DB_NAME")
+    ));
+    config.setUsername(System.getenv("DB_USER")); // e.g. "root", "mysql"
+    config.setPassword(System.getenv("DB_PASS")); // e.g. "my-password"
+    pool = new HikariDataSource(config);
+    createTable(pool);
+    System.setProperty("testingTableName", tableName);
+  }
+
+  @AfterClass
+  public static void dropTable() throws SQLException {
+    try (Connection conn = pool.getConnection()) {
+      String stmt = String.format("DROP TABLE %s;", tableName);
+      try (PreparedStatement createTableStatement = conn.prepareStatement(stmt);) {
+        createTableStatement.execute();
+      }
+    }
+    System.clearProperty("testingTableName");
+  }
+
+  @Test
+  public void testGetTemplateData() throws Exception {
+    TemplateData templateData = new IndexServlet().getTemplateData(pool, tableName);
+
+    assertNotNull(templateData.tabCount);
+    assertNotNull(templateData.spaceCount);
+    assertNotNull(templateData.recentVotes);
+  }
+
+  @Test
+  public void testServletPost() throws Exception {
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    ServletContext context = mock(ServletContext.class);
+
+    when(request.getServletContext()).thenReturn(context);
+    when(context.getAttribute("my-pool")).thenReturn(pool);
+    when(request.getParameter("team")).thenReturn("TABS");
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(response.getWriter()).thenReturn(writer);
+
+    new IndexServlet().doPost(request, response);
+
+    writer.flush();
+    assertTrue(stringWriter.toString().contains("Vote successfully cast for"));
+  }
+}
